2506ff2f8f146f7c4235b0337e8d578993ed36d9
ThatTanna <tanna1987@gmail.com>
2024-05-29 16:44:52 +0800

    test commit

--- src/main/java/com/seoulful/snack/controller/ProductController.java
+++ src/main/java/com/seoulful/snack/controller/ProductController.java
@@ -14,28 +14,42 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.lang.module.ResolutionException;
 import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/products")
-
+@RequestMapping("/admin/api/product")
 public class ProductController {
     @Autowired
     ProductRepository productRepository;
+
+    private final Path fileStorageLocation = Paths.get("./profile_photos").toAbsolutePath().normalize();
+
+    public ProductController() {
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        } catch (Exception ex) {
+            throw new RuntimeException("Could not create the directory where the uploaded files will be stored.", ex);
+        }
+    }
 
     // Add a new product
     @PostMapping(path = "/add")  // Specific path for adding products
     public ResponseEntity<Object> addNewProduct(@Valid @RequestBody Product product) throws Exception {
         try {
-            Product newProduct = new Product(   // Modify constructor based on your Product class
+
+            Product newProduct = new Product( // Modify constructor based on your Product class
                     product.getName(),
                     product.getDescription(),
                     product.getPrice(),
                     "");
+
             Product savedProduct = productRepository.save(newProduct);
 
-            if(savedProduct.getId() == 0)
+            if(savedProduct.getId() <= 0)
                 throw new ResourceNotFoundException("Unable to create product.");
 
             return new ResponseEntity<>("Product added successfully.", HttpStatus.CREATED);
@@ -44,5 +58,8 @@ public class ProductController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
-}
 
+    // TODO - update product
+    // TODO - delete product
+    // TODO - get product by id
+}
